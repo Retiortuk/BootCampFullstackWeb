4ddee0b7452a0f348a696269a7b7578ed336495c
Retiortuk <aauliagazzam@gmail.com>
2024-08-19 18:41:35 +0700

    feat: comment updated

--- 101 JS/external-js/script-2.js
+++ 101 JS/external-js/script-2.js
@@ -3,8 +3,8 @@
 var example = "none";
 
 if (example == 'none') {
-    let mangoes = "10"; /* LET is a Like Variable but The data is changable, but only can works inside of action IF ELSE */
-    const height = "178"; /* CONST is a Like Variable but it's Unchagable, but only can works inside of action IF ELSE*/
+    let mangoes = "10"; /* LET is a Like Variable but The data is changable, but only can works inside of action IF ELSE also works outside of IF ELSE on certain Conditions */
+    const height = "178"; /* CONST is a Like Variable but it's Unchagable, but only can works inside of action IF ELSE also works outside of IF ELSE on certain Conditions*/
     console.log("I do have ", mangoes, "Mangoes")
     console.log("I my height is ", height, "cm")
 }

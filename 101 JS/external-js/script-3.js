12299e3dda3976a4c32f32d98394e4f25abfd77a
Retiortuk <aauliagazzam@gmail.com>
2024-08-19 19:20:45 +0700

    feat: operator JS latihan

---
+++ 101 JS/external-js/script-3.js
@@ -0,0 +1,35 @@
+/* Operator In Javascript */
+
+/* * = Perkalian | - = Pengurangan | + = Pertambahan | / = Pembagian | ** = Pemangkatan | % = Sisa bagi*/
+
+var contohTambah = "3 + 4 = ? type 'tambah' To see the Result";
+var contohKurang = "50 - 10 = ? type 'kurang' To see the Result";
+var contohKali = "510 x 100 = ? type 'kali' To see the Result";
+var contohBagi = "50 : 100 = ? type 'bagi' To see the Result";
+var contohPangkat = "50 ^ 2 = ? type 'pangkat' To see the Result";
+
+console.log(contohTambah);
+console.log(contohKurang);
+console.log(contohKali);
+console.log(contohBagi);
+console.log(contohPangkat);
+
+var aTambah = 3;
+var bTambah= 4;
+var tambah = aTambah + bTambah;
+
+var aKurang = 50;
+var bKurang= 10;
+var kurang = aKurang - bKurang;
+
+var aKali = 510;
+var bKali= 100;
+var kali = aKali * bKali;
+
+var aBagi = 50;
+var bBagi= 100;
+var bagi = aBagi / bBagi;
+
+var aPangkat = 50;
+var bPangkat = 2;
+var pangkat = aPangkat ** bPangkat;

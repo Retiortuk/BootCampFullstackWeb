993ed932ce02c7290005ae5524cc55e1439fef73
Retiortuk <aauliagazzam@gmail.com>
2024-08-19 16:02:37 +0700

    feat: let and const  in js

---
+++ 101 JS/external-js/script-2.js
@@ -0,0 +1,10 @@
+/* Let And Const */
+
+var example = "none";
+
+if (example == 'none') {
+    let mangoes = "10"; /* LET is a Like Variable but The data is changable, but only can works inside of action IF ELSE */
+    const height = "178"; /* CONST is a Like Variable but it's Unchagable, but only can works inside of action IF ELSE*/
+    console.log("I do have ", mangoes, "Mangoes")
+    console.log("I my height is ", height, "cm")
+}

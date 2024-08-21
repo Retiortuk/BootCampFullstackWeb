febcc41a8f5f8e3aca010a6f03f257ddfd31a1bf
Retiortuk <aauliagazzam@gmail.com>
2024-08-21 10:53:59 +0700

    feat: sisah bai added

--- 101 JS/external-js/script-3.js
+++ 101 JS/external-js/script-3.js
@@ -7,12 +7,14 @@ var contohKurang = "50 - 10 = ? type 'kurang' To see the Result";
 var contohKali = "510 x 100 = ? type 'kali' To see the Result";
 var contohBagi = "50 : 100 = ? type 'bagi' To see the Result";
 var contohPangkat = "50 ^ 2 = ? type 'pangkat' To see the Result";
+var contohSisaBagi = "100 Sisah Bagi 1 = ? type 'sisah' To see the Result";
 
 console.log(contohTambah);
 console.log(contohKurang);
 console.log(contohKali);
 console.log(contohBagi);
 console.log(contohPangkat);
+console.log(contohSisaBagi);
 
 var aTambah = 3;
 var bTambah= 4;
@@ -33,3 +35,7 @@ var bagi = aBagi / bBagi;
 var aPangkat = 50;
 var bPangkat = 2;
 var pangkat = aPangkat ** bPangkat;
+
+var aSisahBagi = 100;
+var bSisahBagi = 1;
+var sisah = aSisahBagi % bSisahBagi;

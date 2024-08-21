1972ce196e9d4686ccb9e97d00c86bb56da63366
Retiortuk <aauliagazzam@gmail.com>
2024-08-21 15:14:38 +0700

    feat: select element by their ids

---
+++ 101 JS/external-js/script-5.js
@@ -0,0 +1,8 @@
+/* Select ID Elements from html in JS */
+
+const something = document.getElementById("something")
+
+// something / will show the path 
+
+// something.innerText = "How You Doing Mr" / will change the inside element in HTML id='something'
+

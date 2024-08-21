0265d830eb70c201bc4c5568acad1a1bada6a52f
Retiortuk <aauliagazzam@gmail.com>
2024-08-21 14:53:55 +0700

    feat: condition in JS

---
+++ 101 JS/external-js/script-4.js
@@ -0,0 +1,32 @@
+/* CONDITION IN JAVASCRIPT */
+
+// IF, ELSE IF , ELSE
+
+//IF MY GRADE GREATER THAN 80 I GOT A
+const grade = 90 ;
+
+if (grade >= 80) {
+    console.log("Congrats You've Got 'A'")
+} 
+
+else if (grade <= 80) {
+    console.log("Cool You've Got 'B'")
+}
+
+else if (grade >= 65) {
+    console.log("Okay You've Got 'C'")
+}
+
+else {
+    console.log("Your Grade Below 'C' Keeps Learning")
+}
+
+// Welcoming User
+
+const user = 'John';
+
+if (user == 'Gesa') {
+    console.log("Hallo,", user)
+} else {
+    console.log("Hello, User")
+};

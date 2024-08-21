ad44f86e1191d1cd2465e950eaaa9445228e073d
Retiortuk <aauliagazzam@gmail.com>
2024-08-21 15:28:28 +0700

    feat: object element

---
+++ 101 JS/external-js/script-6.js
@@ -0,0 +1,13 @@
+/* How To Use Object Element  */
+
+const student = {
+    'name': 'Johnny',
+    'id': '123456789',
+    'class': '07A'
+}
+
+// How To Call it
+
+student ['name'] // to see name
+student ['id'] // to see the id
+student ['class'] // to see the class

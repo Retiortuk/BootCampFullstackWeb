e9b25680fcc96716987ca95db87df25f8c92ff4b
Retiortuk <aauliagazzam@gmail.com>
2024-08-21 12:55:43 +0700

    feat: data types in JS

---
+++ 101 JS/external-js/data-types.js
@@ -0,0 +1,15 @@
+/* DATA TYPES IN JAVASCRIPT */
+
+const str = 'string'; // String 
+const num = 9; // Numeral or Number/Integer
+const flt = 3.5; // Float Value Such 2.5 or 3.599
+const bool = true // "False"
+const list = ["Jacob", 12, "Helloi", 13]; // Data Array Or List
+const thing = null; //Has Nothing In it 
+const notDefined = undefined; // currently holds no value
+const getAge = function() {return 31 ;} // This is a Function
+const obj = {
+    'name': 'gesa',
+    'profession': 'student',
+    'address': '5th St Avenue'
+} // obj can be accessed with obj['name]

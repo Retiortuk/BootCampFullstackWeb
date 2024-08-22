74ea0c2443c275430a3368247c119151afdfe583
Retiortuk <aauliagazzam@gmail.com>
2024-08-22 11:26:43 +0700

    feat: string methods

--- 101 JS/external-js/script-7.js
+++ 101 JS/external-js/script-7.js
@@ -1,13 +1,25 @@
 /* String Methods */
 
 
-// .search() = For Searching
-// .slice() = 
-// .substr() = 
+// .search() = For Searching / Start from zero, space includes counted /
+// .length = To know the length Start from zero, space includes counted
+// .slice() =  it will delete the data or slice it
+// .substr() = t will only showing the data that it start and end in parameter
 // .replace() = For Replace 
 // .toUppercase() = For Uppercase 
 // .toLowercase() = For Lowercase 
-// .trim() = 
-// .split() =
+// .trim() = To Delete Unwanted Spaces
+// .split() = To make each sentence to be an array or object data
 
 let car = "Nissan Skyline GT-R R34 1999"
+
+// car.search("Skyline") // Searching
+// car.length // lentgh of data
+// car.slice(1, 25) // it will delete the data or slice it
+// car.substring(1, 14) // it will only showing the data that it start and end in parameter
+// car.replace("which data you want to change type here", "new data that will insert to car data")
+// car.toUpperCase('Nissan')
+// car.toLowerCase('Nissan')
+// car.trim() // To Delete Unwanted Spaces
+// data = car.split(" ") // To make each sentence to be an array or object data
+// typeof(variable) = to see the type of element

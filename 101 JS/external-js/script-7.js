42dff72c69cc13af463b6d6007fc9b6adc682d11
Retiortuk <aauliagazzam@gmail.com>
2024-08-22 10:50:45 +0700

    feat: string methods

---
+++ 101 JS/external-js/script-7.js
@@ -0,0 +1,13 @@
+/* String Methods */
+
+
+// .search() = For Searching
+// .slice() = 
+// .substr() = 
+// .replace() = For Replace 
+// .toUppercase() = For Uppercase 
+// .toLowercase() = For Lowercase 
+// .trim() = 
+// .split() =
+
+let car = "Nissan Skyline GT-R R34 1999"

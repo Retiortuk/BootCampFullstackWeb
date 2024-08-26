c33ea2e72b49c289b225eb0b3ee08e850e5c0c41
Retiortuk <aauliagazzam@gmail.com>
2024-08-26 14:39:21 +0700

    feat: Multiple dom nodes

---
+++ 101 JS/external-js/script-8.js
@@ -0,0 +1,18 @@
+
+/* To Select Multiple DOM  */
+
+const elms = document.querySelectorAll(".this-class");  // querySelectorAll() = For Select All Class Not Just the First one
+console.log(elms)
+
+/* Select List */
+const lst = document.querySelectorAll("li");
+
+
+/* For Show Index Sorted Started From 0 */
+// lst.forEach((node, index) => {
+//     node.innerText = 'Hello This is' + index });
+
+/* To Add Custom Class */
+// lst.forEach(node => {
+//     node.classList.add("Custom-class","Second-class") -> THIS WIL MAKE NEW 2 CLASS
+// });

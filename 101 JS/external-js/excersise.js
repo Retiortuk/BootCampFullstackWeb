9735750bbb224e45242b5b3e1c8c81761e84b151
Retiortuk <aauliagazzam@gmail.com>
2024-08-30 18:20:49 +0700

    feat: calculator finished

---
+++ 101 JS/external-js/excersise.js
@@ -0,0 +1,22 @@
+// Simple Calculator In JS
+
+let num1 = parseFloat(prompt("Enter The First Number: "));
+let operator = prompt("Enter The Operator +, x, -, :, = ");
+let num2 = parseFloat(prompt("Enter The Second Number: "));
+
+let result;
+
+if (operator === "+") {
+    result = num1 + num2;
+} else if (operator === "x") {
+    result = num1 * num2;
+} else if (operator === "-") {
+    result = num1 - num2;
+} else if (operator === ":") {
+    result = num1 / num2;
+} else {
+    result = "Selecet Available Operator";
+}
+
+alert("The result is: " + result);
+

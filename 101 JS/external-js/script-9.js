1d6fc46c5acecbff1a4b476a8556368cfd23368f
RetiortUK <aauliagazzam@gmail.com>
2024-09-02 20:22:44 -0700

    feat: function js

--- 101 JS/external-js/script-9.js
+++ 101 JS/external-js/script-9.js
@@ -1,6 +1,24 @@
-function addNumbers() {
-    //Logic
-    return 'an answer';
-}
+// function addNumbers() {
+//     //Logic
+//     return 'an answer';
+// }
 
-let total = addNumbers()
+// let total = addNumbers()
+
+// function
+
+
+/* NUMERAL */
+function timesNumbers(no1, no2) {
+    const total = Number(no1) * Number(no2);
+    return total;
+}
+const timesNumbersTotal = timesNumbers('100', '40');
+
+/* WORD */
+function greeting(name) {
+    const hi = 'Hello ' + name
+    return hi;
+}
+const hello = greeting('Gesa')
+

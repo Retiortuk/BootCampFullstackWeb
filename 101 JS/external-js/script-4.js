a8a1a45fd79decc7836cafede58398d2c197d9a2
RetiortUK <aauliagazzam@gmail.com>
2024-11-03 14:55:42 +0700

    im back

--- 101 JS/external-js/script-4.js
+++ 101 JS/external-js/script-4.js
@@ -3,27 +3,42 @@
 // IF, ELSE IF , ELSE
 
 //IF MY GRADE GREATER THAN 80 I GOT A
-const grade = 90 ;
+// const grade = 50 ;
 
-if (grade >= 80) {
-    console.log("Congrats You've Got 'A'")
-} 
+// if (grade >= 80) {
+//     console.log("Congrats You've Got 'A'")
+// } 
 
-else if (grade <= 80) {
-    console.log("Cool You've Got 'B'")
-}
+// else if (grade <= 80) {
+//     console.log("Cool You've Got 'B'")
+// }
 
-else if (grade >= 65) {
-    console.log("Okay You've Got 'C'")
-}
+// else if (grade >= 65) {
+//     console.log("Okay You've Got 'C'")
+// }
 
-else {
-    console.log("Your Grade Below 'C' Keeps Learning")
+// else {
+//     console.log("Your Grade Below 'C' Keeps Learning")
+// }
+
+const nilai = 50;
+
+if (nilai >= 85){
+    console.log("Selamat Anda Dapat A")
+
+} else if (nilai >= 70){
+    console.log("Bagus Kamu Dapat Nilai B")
+
+} else if (nilai >= 60) {
+    console.log("Belajar Lagi ya Kamu Dapat C")
+
+} else {
+    console.log("Banyak Banyak Belajar Ya")
 }
 
 // Welcoming User
 
-const user = 'John';
+const user = 'Gesa';
 
 if (user == 'Gesa') {
     console.log("Hallo,", user)

3432227c205692f122dd5c0fe18bbbd5430b457f
Retiortuk <aauliagazzam@gmail.com>
2025-06-25 22:50:08 +0700

    feat: react import component

--- 101-react/my-app/src/App.js
+++ 101-react/my-app/src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './Myitem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi Gesa
-        </p>
+        <Item />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ 101-react/my-app/src/Myitem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Item extends React.Component {
+    render () {
+        return (
+            <h1>Hello From Gesa</h1>
+        )
+    }
+}
+
+// Dont forget to export it first so that it can be used for import
+export default Item;

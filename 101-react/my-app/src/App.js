6f31e30ed42639c5f60baee1397950fab762a7eb
Retiortuk <aauliagazzam@gmail.com>
2025-06-26 13:38:50 +0700

    feat: react star wars component

--- 101-react/my-app/src/App.js
+++ 101-react/my-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import Item from './Myitem';
+import React from 'react'
+
+class StarWars extends React.Component {
+  render () {
+    return (
+      <h1>This is Star Wars</h1>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         {/* React js Props */}
-        <Item name="Gesa" Time="Good Morning"/>
-        <Item name="Gesa" Time="Good Morning"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.js
-        </a>
+        {/* <Item name="Gesa" Time="Good Morning"/> */}
+        <StarWars />
       </header>
     </div>
   );

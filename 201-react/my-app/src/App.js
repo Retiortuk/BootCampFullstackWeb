d50c2cc2849a25333a2362a5c313e8262d082fab
Retiortuk <aauliagazzam@gmail.com>
2025-06-28 17:43:58 +0700

    feat: Components About

--- 201-react/my-app/src/App.js
+++ 201-react/my-app/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
+
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar />
       {/* Home Dari Import Dari components/Navbar.js */}
       <Home />
+      {/* About Dari Import Dari components/About.js */}
+      <About />
     </div>
   );
 }

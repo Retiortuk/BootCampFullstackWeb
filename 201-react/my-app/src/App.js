21a7a4a3babc22826ddad6da6d6c1a5dbfbcdd02
Retiortuk <aauliagazzam@gmail.com>
2025-07-03 20:47:35 +0700

    feat: mapping

--- 201-react/my-app/src/App.js
+++ 201-react/my-app/src/App.js
@@ -11,15 +11,20 @@ function App() {
   const [searchResults, setSearchResults] = useState([]); // State untuk menyimpan hasil pencarian  
   const [searchText, setSearchText] = useState(''); // State untuk menyimpan teks pencarian
   
+  
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=20c140e4adb91f55d3869a54318cf4b4&query=${searchText}`)
-      .then(response => response.json() // Mengambil data dari API
-      .then(data => {
-        console.log("Data:", data);
-        setSearchResults(data.results);
-        // console.log(searchResults);
-      }));
 
+    // useEffect untuk menangani efek samping, seperti pencarian ketika searchText berubah
+    // Jika searchText tidak kosong, maka akan melakukan pencarian
+    if(searchText) {
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=20c140e4adb91f55d3869a54318cf4b4&query=${searchText}`)
+        .then(response => response.json() // Mengambil data dari API
+        .then(data => {
+          console.log("Data:", data);
+          setSearchResults(data.results);
+          // console.log(searchResults);
+        }));
+    }
   }, [searchText]); // useEffect untuk menangani efek samping, seperti pencarian ketika searchText berubah
 
   return (
--- 201-react/my-app/src/components/SearchView.js
+++ 201-react/my-app/src/components/SearchView.js
@@ -6,10 +6,21 @@ import Hero from "./Hero";
 const SearchView = ({keyword, searchResults}) => { // Menerima props keyword dan searchResults dari App.js
     // keyword adalah teks yang dicari, searchResults adalah hasil pencarian
     const title = `you search for ${keyword}`; // Judul yang akan ditampilkan di Hero, bisa diubah sesuai kebutuhan
+
+    const ResultsHTML = searchResults.map((obj, index) => { // Menggunakan map untuk mengiterasi setiap hasil pencarian
+        // obj adalah setiap objek film dari hasil pencarian, index adalah indeksnya
+        return (
+            <div key={index}>
+                {obj.title} {/* Menampilkan judul film dari hasil pencarian */}
+            </div>
+        )
+    }) 
+
     return (
         <>
             {/* Menampilkan Hero dengan teks yang sesuai */}
             <Hero text={title} /> {/*text berupa seperti nama class yang bisa dipanggil di Hero.js  */}
+            {ResultsHTML} {/* Menampilkan hasil pencarian */}
         </>
     );
 };
